5ad93a291137269c28c0ff97eeac592cf6c5f788
dreo <79935220+4ndreo@users.noreply.github.com>
2024-01-16 19:49:12 -0300

    feat: imagenes den pricing cards

--- src/components/pricing-cards.tsx
+++ src/components/pricing-cards.tsx
@@ -1,7 +1,7 @@
+'use client'
+
 import styles from '../components/pricing-cards.module.css';
-import smallComp from '@/utils/img/pequeñas-empresas.svg';
-import bigComp from '@/utils/img/grandes-empresas.svg';
-import persPlan from '@/utils/img/plan-personalizado.svg';
+import {Image} from "@nextui-org/react";
 
 const PricingCard = ({
 	type,
@@ -17,7 +17,11 @@ const PricingCard = ({
 	alt: string
 }) => (
 	<div className={styles.card + " flex flex-col relative p-5 h-full mx-auto max-w-sm items-center bg-white rounded-lg border border-gray-300 shadow xl:p-8"}>
-		<img src={source} alt={alt} width={353} height={202}/>
+		{/* <img src={source} alt={alt} width={353} height={202}/> */}
+		<Image
+      alt={alt} width={190} height={150}
+      src={source}
+    />
 		<h3 className={styles.type}>{type}</h3>
 		<p className={styles.text+" sm:text-lg"}>{description}</p>
 		<a href="" className= {styles.bttn +" border border-gray-900 rounded-lg py-2.5 text-center"}>
@@ -32,21 +36,21 @@ const PricingCards = () => {
 			type: 'Pequeña Escala',
 			description: 'Transforma tu negocio o emprendimiento con nuestros servicios digitales diseñados a medida. Desde presencia en internet hasta soluciones de comercio electrónico y marketing digital, te ofrecemos un conjunto integral de herramientas para expandir tu alcance, mejorar la visibilidad y aumentar tus ingresos.',
 			ctaText: 'Elegir plan',
-			source: smallComp,
+			source: '/img/pequeñas-empresas.svg',
 			alt: 'pequeña escala',
 		},
 		{
 			type: 'Grandes Empresas',
 			description: 'Impulsa el crecimiento de tu empresa con nuestros servicios digitales de vanguardia. Nuestra gama completa de soluciones incluye desarrollo de software personalizado, estrategias de transformación digital y gestión de proyectos tecnológicos. Maximiza la eficiencia, optimiza tus procesos y asegura un crecimiento sostenible en el mundo digital.',
 			ctaText: 'Elegir plan',
-			source: bigComp,
+			source: '/img/grandes-empresas.svg',
 			alt: 'grandes empresas',
 		},
 		{
 			type: 'Plan Personalizado',
 			description: 'Crea un camino único hacia el éxito con nuestro plan personalizado. Trabajaremos estrechamente contigo para entender tus objetivos, desafíos y visiones específicas. Desde la consultoría estratégica hasta la implementación de soluciones personalizadas, te proporcionaremos un plan digital que se adapte perfectamente a tus necesidades individuales, permitiéndote alcanzar tus metas de manera efectiva y a tu manera.',
 			ctaText: 'Elegir plan',
-			source: persPlan,
+			source: '/img/plan-personalizado.svg',
 			alt: 'plan personalizado',
 		}
 	]
