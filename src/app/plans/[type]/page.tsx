fbf66728a016796bcd05591de89cd4c4ad048f14
sotoplasma <jeresoto4@gmail.com>
2024-01-18 16:32:15 -0300

    feat: página de planes fix: pricing cards

--- src/app/plans/[type]/page.tsx
+++ src/app/plans/[type]/page.tsx
@@ -1,24 +1,146 @@
-import Image from 'next/image'
+'use client';
+import React, { useEffect } from "react";
+import {Button, Input, Textarea, Select, SelectItem} from "@nextui-org/react";
+import styles from "@/app/plans/[type]/page.module.css";
 
-import HeroSection from '@/components/hero-section'
-import Introduction from '@/components/introduction'
-import Content from '@/components/content'
-import Contact from '@/components/contact'
-import Header from '@/components/header'
-import PricingCards from '@/components/pricing-cards'
+export default function Plans({params}: any) {
 
-export default function Plans({type}: any) {
+	const rubros = ["gastronomía", "salud"];
+	const servicios = ["Desarrollo web", "Desarrollo de APIs", "Marketing digital"];
+	const nEmpleados = ["1 a 10", "10 a 50", "50 a 200", "+200"];
+
+	const [formEmpresa, setFormEmpresa] = React.useState('');
+	const [formCantidad, setFormCantidad] = React.useState('');
+	const [formRubro, setFormRubro] = React.useState('');
+	const [formServicio, setFormServicio] = React.useState('');
+	const [formObjetivo, setFormObjetivo] = React.useState('');
+	const [formNombre, setFormNombre] = React.useState('');
+	const [formTel, setFormTel] = React.useState('');
+	const [formEmail, setFormEmail] = React.useState('');
+
+	const type = params.type.replace("_", " ").replace("%C3%B1", "ñ");
+	const [bgType, setBgType] = React.useState('');
+
+	function switchBg () {
+		if (type === 'Grandes Empresas') {
+			setBgType("bgBig")
+		}
+		if (type === 'Personalizado') {
+			setBgType("bgCustom")
+		}
+		if (type === 'Pequeña Escala') {
+			setBgType("bgSmall")
+		}
+	}
+	useEffect(()=>{
+		switchBg()
+	},[])
+
+	function validateInput (param: string): boolean {
+
+		return typeof param === "string" && param.length > 0 && param.length < 150 ? false : true
+	}
+
+	function validatePhone (param: number): boolean {
+		return typeof param === "number" && param >= 1000000 ? false : true
+	}
+
+	function validateEmail (param: string): boolean {
+		return typeof param === "string" && param.includes("@") ? false : true
+	}
+
 	return (
-		<main>
-			<h1>{type}</h1>
-			<Header />
-			<HeroSection />
-			<div className="grey">
-			<Introduction />
-			<Content />
-			</div>
-			<PricingCards />
-			<Contact />
-		</main>
+		<section className="bg-black">
+				<div className={styles[bgType] +" max-w-7xl m-auto text-white"}>
+					<div className={styles.intro+" pt-40"}>
+						<h2 className={styles.title}>Elegiste el plan "{type}"</h2>
+						<p className={styles.text}>Por favor danos más información para poder brindarte el mejor servicio</p>
+					</div>
+					<div className="max-w-[45%]">
+						<form>
+							<h3 className={styles.sectionTitle}>Empresa</h3>
+							<div className={styles.sectionForm}>
+								<div className="flex w-full flex-wrap md:flex-nowrap gap-10 mb-6">
+									<Input
+										type="text"
+										label="Empresa"
+										variant="bordered"
+										classNames={{label: "!text-white", inputWrapper: "!border-white"}}
+										onBlur={(e: any)=>{setFormEmpresa(e.target.value)}}
+										isInvalid={validateInput(formEmpresa)}
+										color={validateInput(formEmpresa) ? "danger" : "success"} />
+									<Select 
+										label="Cantidad de empleados" 
+										variant="bordered"
+										selectionMode="single"
+										classNames={{label: "!text-white", trigger: "!border-white"}} >
+											{nEmpleados.map((cantidad: string) => (
+												<SelectItem key={cantidad} value={cantidad}>
+													{cantidad}
+												</SelectItem>
+											))}
+									</Select>
+								</div>
+
+								<div className="flex w-full flex-wrap md:flex-nowrap">
+									<Select 
+										label="Rubro" 
+										className="w-full"
+										variant="bordered"
+										classNames={{label: "!text-white", trigger: "!border-white"}} >
+										{rubros.map((rubro: string) => (
+											<SelectItem key={rubro} value={rubro}>
+												{rubro}
+											</SelectItem>
+										))}
+									</Select>
+								</div>
+							</div>
+
+							<h3 className={styles.sectionTitle}>Servicio</h3>
+							<div className={styles.sectionForm}>
+								<div className="flex w-full flex-wrap md:flex-nowrap mb-3">
+										<Select 
+											label="¿Qué servicio podemos ofrecerte?" 
+											className="w-full"
+											variant="bordered"
+											selectionMode="multiple"
+											description="Podés elegir más de uno"
+											classNames={{label: "!text-white", trigger: "!border-white"}} >
+											{servicios.map((servicio: string) => (
+												<SelectItem key={servicio} value={servicio}>
+													{servicio}
+												</SelectItem>
+											))}
+										</Select>
+								</div>
+					
+								<Textarea
+									variant="bordered"
+									label="¿Cuál es tu objetivo?"
+									labelPlacement="inside"
+									minRows={50}
+									classNames={{
+										label: "!text-white",
+										inputWrapper: "!border-white",
+									  }}
+									className="flex flex-wrap md:flex-nowrap w-auto" />
+
+							</div>
+
+							<h3 className={styles.sectionTitle}>Información de contacto</h3>
+							<div className={styles.sectionForm}>
+								<div className="flex w-full flex-wrap md:flex-nowrap gap-10 mb-6">
+									<Input type="text" label="Nombre" variant="bordered" classNames={{label: "!text-white", inputWrapper: "!border-white"}} />
+									<Input type="phone" label="Teléfono" variant="bordered" classNames={{label: "!text-white", inputWrapper: "!border-white"}} />
+								</div>
+								<Input type="email" label="Email" variant="bordered" className="flex w-full flex-wrap md:flex-nowrap" classNames={{label: "!text-white", inputWrapper: "!border-white"}} />
+							</div>
+
+							<Button className={styles.bttn+" w-full mb-12"}>Enviar</Button>
+						</form>
+					</div>
+				</div>
+		</section>
 	)
 }
--- src/components/pricing-cards.tsx
+++ src/components/pricing-cards.tsx
@@ -9,12 +9,14 @@ const PricingCard = ({
 	ctaText,
 	source,
 	alt,
+	name,
 }: {
 	type: string
 	description: string
 	ctaText: string
 	source: string
 	alt: string
+	name: string
 }) => (
 	<div className={styles.card + " flex flex-col relative p-5 h-full mx-auto max-w-sm items-center bg-white rounded-lg border border-gray-300 shadow xl:p-8"}>
 		{/* <img src={source} alt={alt} width={353} height={202}/> */}
@@ -22,36 +24,39 @@ const PricingCard = ({
 		alt={alt} width={'auto'} height={155}
 		src={source}
 		/>
-		<h3 className={styles.type}>{type}</h3>
+		<h3 className={styles.cardName}>{name}</h3>
 		<p className={styles.text+" sm:text-lg"}>{description}</p>
-		<Link as={Button} href={'/plans/' + type} className={styles.bttn +" rounded-lg py-2.5 text-center justify-center"}>
+		<Button as={Link} href={'/plans/' + type} className={styles.bttn +" rounded-lg py-2.5 text-center justify-center"}>
 			{ctaText}
-		</Link>
+		</Button>
 	</div>
 )
 
 const PricingCards = () => {
 	const cards = [
 		{
-			type: 'Pequeña Escala',
+			type: 'Pequeña_Escala',
 			description: 'Transforma tu negocio o emprendimiento con nuestros servicios digitales diseñados a medida. Desde presencia en internet hasta soluciones de comercio electrónico y marketing digital, te ofrecemos un conjunto integral de herramientas para expandir tu alcance, mejorar la visibilidad y aumentar tus ingresos.',
 			ctaText: 'Elegir plan',
 			source: '/img/pequeñas-empresas.svg',
 			alt: 'pequeña escala',
+			name: 'Pequeña Escala',
 		},
 		{
-			type: 'Grandes Empresas',
+			type: 'Grandes_Empresas',
 			description: 'Impulsa el crecimiento de tu empresa con nuestros servicios digitales de vanguardia. Nuestra gama completa de soluciones incluye desarrollo de software personalizado, estrategias de transformación digital y gestión de proyectos tecnológicos. Maximiza la eficiencia, optimiza tus procesos y asegura un crecimiento sostenible en el mundo digital.',
 			ctaText: 'Elegir plan',
 			source: '/img/grandes-empresas.svg',
 			alt: 'grandes empresas',
+			name: 'Grandes Empresas',
 		},
 		{
-			type: 'Plan Personalizado',
+			type: 'Personalizado',
 			description: 'Crea un camino único hacia el éxito con nuestro plan personalizado. Trabajaremos estrechamente contigo para entender tus objetivos, desafíos y visiones específicas. Desde la consultoría estratégica hasta la implementación de soluciones personalizadas, te proporcionaremos un plan digital que se adapte perfectamente a tus necesidades individuales, permitiéndote alcanzar tus metas de manera efectiva y a tu manera.',
 			ctaText: 'Elegir plan',
 			source: '/img/plan-personalizado.svg',
 			alt: 'plan personalizado',
+			name: 'Personalizado',
 		}
 	]
 
