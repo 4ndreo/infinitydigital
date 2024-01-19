506bb6e51c065d5eec69be08c8cbf49ca5cb9afe
dreo <79935220+4ndreo@users.noreply.github.com>
2024-01-18 21:33:30 -0300

    feat: validacion parcial de formulario

--- src/app/plans/[type]/page.tsx
+++ src/app/plans/[type]/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useEffect } from "react";
+import React, { use, useEffect } from "react";
 import {Button, Input, Textarea, Select, SelectItem} from "@nextui-org/react";
 import styles from "@/app/plans/[type]/page.module.css";
 
@@ -9,14 +9,15 @@ export default function Plans({params}: any) {
 	const servicios = ["Desarrollo web", "Desarrollo de APIs", "Marketing digital"];
 	const nEmpleados = ["1 a 10", "10 a 50", "50 a 200", "+200"];
 
-	const [formEmpresa, setFormEmpresa] = React.useState('');
-	const [formCantidad, setFormCantidad] = React.useState('');
-	const [formRubro, setFormRubro] = React.useState('');
-	const [formServicio, setFormServicio] = React.useState('');
-	const [formObjetivo, setFormObjetivo] = React.useState('');
-	const [formNombre, setFormNombre] = React.useState('');
-	const [formTel, setFormTel] = React.useState('');
-	const [formEmail, setFormEmail] = React.useState('');
+	const [values, setValues] = React.useState({
+		empresa: "",
+		nEmpleados: "",
+		rubro: "",
+		servicio: [""],
+		name: "",
+		phone: "",
+		email: "",
+	})
 
 	const type = params.type.replace("_", " ").replace("%C3%B1", "ñ");
 	const [bgType, setBgType] = React.useState('');
@@ -32,13 +33,33 @@ export default function Plans({params}: any) {
 			setBgType("bgSmall")
 		}
 	}
+
+	function handleChange (e: any) {
+		setValues({...values, [e.target.name ?? e[0]]: e.target.value})
+	}
+
+	function handleSelectionChange ( name: string, e: any) {
+		console.log('AAAA', e.target.value.split(" ,"));
+		setValues({...values, [name]: e.target.value.split(" ,")})
+	}
+
+	function handleMultipleSelectionChange ( name: string, e: any) {
+		console.log('parametro', e.target.value.split(","));
+		setValues({...values, [name]: e.target.value.split(",")})
+	}
+	
 	useEffect(()=>{
 		switchBg()
 	},[])
+	
+	useEffect(()=>{
+		console.log(values);
+		
+	}, [values])
 
-	function validateInput (param: string): boolean {
-
-		return typeof param === "string" && param.length > 0 && param.length < 150 ? false : true
+	function isString (param: string): boolean {
+		// console.log('parametro', param);
+		return typeof param === "string" && param.length >= 0 && param.length < 150 ? true : false
 	}
 
 	function validatePhone (param: number): boolean {
@@ -62,17 +83,22 @@ export default function Plans({params}: any) {
 							<div className={styles.sectionForm}>
 								<div className="flex w-full flex-wrap md:flex-nowrap gap-10 mb-6">
 									<Input
+										isRequired
 										type="text"
+										name="empresa"
 										label="Empresa"
 										variant="bordered"
 										classNames={{label: "!text-white", inputWrapper: "!border-white"}}
-										onBlur={(e: any)=>{setFormEmpresa(e.target.value)}}
-										isInvalid={validateInput(formEmpresa)}
-										color={validateInput(formEmpresa) ? "danger" : "success"} />
+										onBlur={(e: any)=>{handleChange(e)}}
+										isInvalid={!isString(values.empresa)}
+										color={isString(values.empresa) ? "success" : "danger" } 
+										errorMessage={!isString(values.empresa) && "Por favor ingresa el nombre de tu empresa"}
+									/>
 									<Select 
 										label="Cantidad de empleados" 
 										variant="bordered"
 										selectionMode="single"
+										onSelectionChange={(e: any)=>{handleSelectionChange('nEmpleados', e)}}
 										classNames={{label: "!text-white", trigger: "!border-white"}} >
 											{nEmpleados.map((cantidad: string) => (
 												<SelectItem key={cantidad} value={cantidad}>
@@ -85,8 +111,10 @@ export default function Plans({params}: any) {
 								<div className="flex w-full flex-wrap md:flex-nowrap">
 									<Select 
 										label="Rubro" 
+										// name="rubro"
 										className="w-full"
 										variant="bordered"
+										onSelectionChange={(e: any)=>{handleSelectionChange('rubro', e)}}
 										classNames={{label: "!text-white", trigger: "!border-white"}} >
 										{rubros.map((rubro: string) => (
 											<SelectItem key={rubro} value={rubro}>
@@ -106,6 +134,7 @@ export default function Plans({params}: any) {
 											variant="bordered"
 											selectionMode="multiple"
 											description="Podés elegir más de uno"
+											onChange={(e: any)=>{handleMultipleSelectionChange('servicio', e)}}
 											classNames={{label: "!text-white", trigger: "!border-white"}} >
 											{servicios.map((servicio: string) => (
 												<SelectItem key={servicio} value={servicio}>
@@ -120,6 +149,10 @@ export default function Plans({params}: any) {
 									label="¿Cuál es tu objetivo?"
 									labelPlacement="inside"
 									minRows={50}
+									onBlur={(e: any)=>{handleChange(e)}}
+									isInvalid={!isString(values.empresa)}
+									color={isString(values.empresa) ? "success" : "danger" } 
+									errorMessage={!isString(values.empresa) && "Por favor ingresa el nombre de tu empresa"}
 									classNames={{
 										label: "!text-white",
 										inputWrapper: "!border-white",
